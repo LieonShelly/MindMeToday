d5be0613bf171d5a0081651c0f5aa74d17a14d0a
lieoncx <lieoncx@gmail.com>
2018-04-26 23:20:40 +0800

    port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var index = require('./routes/index')
+const PORT = process.env.PORT || 3000
+
 
 app.use(bodyParser.json())
 
@@ -11,8 +13,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000 ,() => {
-    console.log('listening on port 3000')
+app.listen(PORT ,() => {
+    console.log('listening on port:' + PORT)
 })
 
 
